3ef8f503e79a0d9dd5ef032dcc056f5f2493334d
hussain kadir memon <hussainkadir245@gmail.com>
2026-07-07 21:19:09 +0500

    updated

--- src/components/ProdCard.jsx
+++ src/components/ProdCard.jsx
@@ -86,7 +86,7 @@ function ProdCard() {
           
           {/* Dynamic bottom laser bar anchor */}
           <span className="absolute bottom-0 left-0 w-full h-[2px] bg-blue-500 scale-x-0 group-hover:scale-x-100 transition-transform duration-300 origin-left" />
-        </button>
+        </button> 
       </div>
 
       {/* RESPONSIVE DESIGN CLUSTER GRID */}
